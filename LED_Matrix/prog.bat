7f28376f9d90a2fb38ac0e6821e549725d630fda
Brian Silverman <bri@raspberrystem.com>
2015-08-13 18:08:33 -0400

    led_matrix - added programming batch file

---
+++ LED_Matrix/prog.bat
@@ -0,0 +1,12 @@
+@echo off
+set PROG="c:\Program Files (x86)\Atmel\Atmel Studio 6.2\atbackend\atprogram.exe"
+set OPTS=-t avrispmk2 --clock 125khz -i isp -d attiny48
+set FASTOPTS=-t avrispmk2 --clock 1000khz -i isp -d attiny48
+set REL_ELF=C:\Users\Brian\Dropbox\raspberrystem\projects\LEDMatrix\src\LED_Matrix_ATINY48\Release\LED_Matrix_ATINY48.elf
+set DBG_ELF="C:\Users\Brian\Dropbox\raspberrystem\projects\LEDMatrix\src\LED_Matrix_ATINY48\Debug\LED_Matrix_ATINY48.elf"
+echo ERASING
+%PROG% %OPTS% chiperase
+echo WRITING FUSES
+%PROG% %OPTS% write -fs --values eedfff 
+echo PROGRAMMING
+%PROG% %FASTOPTS% program --verify -f %REL_ELF%
